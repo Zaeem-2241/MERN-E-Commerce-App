7bef62c43ccacb9108755d502a4ba42c050f39a7
Muhammad Zaeem <zaeemsaeed2241@gmail.com>
2026-04-25 17:27:42 +0500

    deployed

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // ✅ Correct CORS
 app.use(cors({
-  origin: "https://mern-e-commerce-app-rx3m-loulbgzrt-zaeem-2241s-projects.vercel.app",
+  origin: "https://mern-e-commerce-app-rx3m-zaeem-2241s-projects.vercel.app/",
   credentials: true
 }));
 
